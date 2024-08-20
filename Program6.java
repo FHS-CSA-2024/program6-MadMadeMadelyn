3d5047a44177e1db3a21e833d357607706712ca1
MadMadeMadelyn <madelynching916@gmail.com>
2024-08-20 15:30:50 -0700

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -1,7 +1,24 @@
 //import stuff here!
-
+import java.util.Scanner;
 //Your code here
-
+public class Program6 {
+    public static void main(String[]args){
+        double pi = 3.14;
+        double r = 0;
+        double circumference = 2*pi*r;
+        double diameter = 2*r;
+        double area = pi*r*r;
+        
+        Scanner numScanner = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        r = numScanner.nextInt();
+        
+        System.out.println("The Radius of the circle = " + r);
+        System.out.println("The Diameter of the circle = " + diameter);
+        System.out.println("The Area of the circle = " + area);
+        System.out.println("The Circumference of the circle = " + circumference);
+    }
+}
 //Paste console output below:
 /*
 
